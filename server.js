f280b1cc083e04caeeb3219db7ecebb3a4c14663
mahikilari <133624942+mahikilari@users.noreply.github.com>
2024-05-20 15:45:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Worldd');
+  res.send('Hello Worlddfkkhhfrfrfrt');
 });
 
 app.listen(PORT, HOST);
